4247327b1fbb5436f49c67b473201980e277699b
Justin Bradford <jabradford@gmail.com>
2012-03-21 17:58:10 -0700

    Misc cleanup and start on code to support distributed full scans (ala hadoop)

--- test.js
+++ test.js
@@ -5,5 +5,9 @@ var dei = require('.');
 var c = new dei.Connection({});
 c.use('foo');
 var r = c.cql('select * from users');
+var s = new dei.Selector(c);
+
+// var cr = new dei.Connection({port: 9161});
+// cr.use('Lists');
 
 require('ringo/shell').start();
